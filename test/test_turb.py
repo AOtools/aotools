567d65f381ef8e8e53be1c130070108249458674
Alastair Basden <a.g.basden@durham.ac.uk>
2017-12-21 10:09:58 +0000

    debug

--- test/test_turb.py
+++ test/test_turb.py
@@ -1,5 +1,5 @@
 import aotools.turbulence
 
-def test_calcSeeing():
-    res=aotools.turbulence.calcSeeing(0.135,800.,30)
+def test_calc_seeing():
+    res=aotools.turbulence.calc_seeing(0.135,800.,30)
     assert type(res)==type(float)
